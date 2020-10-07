b2154bd5ce6a79cb13ae36e882a28ee60eec8919
Marcelo Jean <marcelojeam1@gmail.com>
2020-10-07 15:39:47 -0300

    Adiciona SQLite como base de dados, e altera o caminho da Fabrica de conexão.

--- src/Infra/Persistence/ConnectionFactory.php
+++ src/Infra/Persistence/ConnectionFactory.php
@@ -11,7 +11,7 @@ class ConnectionFactory
 
     public static function createConnection(): PDO
     {
-        $databasePath = __DIR__ . '../../../banco.sqlite';
+        $databasePath = __DIR__ . '../Database/banco.sqlite';
         $pdo = new PDO('sqlite:' . $databasePath);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
